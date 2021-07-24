f8d4d8df94abc55133077cec8f528338f311ad93
edwint88 <tuzaredwin@gmail.com>
2021-07-24 17:09:50 +0200

    add bullet damage and health takes damage add vscode to gitignore add 0 gravity in physics2dsettings

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField]
+    int damage = 10;
+
     void Update()
     {
         transform.Translate(new Vector3(0, 10, 0) * Time.deltaTime);
+    }
+
+    public int getDamage()
+    {
+        return damage;
     }
 }
